5507fe9bafe9986190351bc8ba81fc283e2a5d65
adeniyitobi055 <61655996+adeniyitobi055@users.noreply.github.com>
2023-09-04 09:32:04 +0100

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -19,7 +19,7 @@ export default function Form({ onAddItems }) {
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😉 tript?</h3>
+      <h3>What do you need for your trip 😉?</h3>
       <select
         value={quantity}
         onChange={(e) => setQuantity(Number(e.target.value))}
